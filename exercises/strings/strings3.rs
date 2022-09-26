1093d500bf9f915e5e774cafb082c56fb93445ad
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-09-26 23:07:17 +0200

    Übertragung Lernvortschritt gitpod

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -5,17 +5,19 @@
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    input.to_string();
+    input.trim();
+    return input;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    format!("{}!",input)
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.replace("cars", "balloons")
 }
 
 #[cfg(test)]
