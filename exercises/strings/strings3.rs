367ec049635676d43f5dcdbbb4a0494e2735e7f5
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-01 11:38:10 +0200

    Übertragung Lernvortschritt gitpod finally

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -1,18 +1,16 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    input.trim_right().chars().rev();
-    let new_string = String::from(input);
+    let string_temp = input.trim();
+    let new_string = String::from(string_temp);
     return new_string;
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    format!("{}!",input)
+    format!("{} world!",input)
 }
 
 fn replace_me(input: &str) -> String {
