73d9c5273bdcc06c0c72465f2baf1c429b967c02
RedSpark <104280152+TheRedSpark@users.noreply.github.com>
2022-10-02 13:13:17 +0200

    Übertragung Lernvortschritt gitpod

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -8,7 +8,8 @@
 // I AM NOT DONE
 
 // TODO: Complete this use statement
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
